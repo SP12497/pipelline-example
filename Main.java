e5865111a9f20fd62ba3e273a0136ec792c18b65
Sagar Patil <SP873747@gmail.com>
2024-03-28 02:25:07 +0530

    jenkins updated

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
+        // ip:8080/env-vars.html    // jenkins variables list
         System.out.println("This is java code main file");
         System.out.println("This is second line of code for jenkins pipeline test");
     }
