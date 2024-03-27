6fdbdab7c9fc72f19515775176d3dca0b8fc2a06
Sagar Patil <SP873747@gmail.com>
2024-03-28 01:55:34 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is java code main file");
+        System.out.println("This is second line of code for jenkins pipeline test");
+    }
+}
